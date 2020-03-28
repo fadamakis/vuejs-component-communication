f5b89261c6176a4735e3024885ba90f71923ae57
Fotis Adamakis <fotis316@yahoo.com>
2020-03-28 16:50:52 +0200

    Event bus component communication

---
+++ src/components/event-bus/ComponentA.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="clickHandler">Send event</button>
+</template>
+
+<script>
+import eventBus from "./event-bus";
+
+export default {
+  methods: {
+    clickHandler() {
+      eventBus.$emit("customEventName", "ping");
+    }
+  }
+};
+</script>
---
+++ src/components/event-bus/ComponentB.vue
@@ -0,0 +1,15 @@
+<template>
+  <section></section>
+</template>
+
+<script>
+import eventBus from "./event-bus";
+
+export default {
+  created() {
+    eventBus.$on("customEventName", payload => {
+      console.log(`Custom event fired with data: ${payload}`);
+    });
+  }
+};
+</script>
