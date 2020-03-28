0b0a8a59c8c8177fbbcf86fe75b537ab9279f6ec
Fotis Adamakis <fotis316@yahoo.com>
2020-03-28 16:50:15 +0200

    3 chain component communication

---
+++ src/components/3-components-chain/ComponentA.vue
@@ -0,0 +1,17 @@
+<template>
+  <ComponentB message="ping" @customEventName="handleCustomEvent" />
+</template>
+
+<script>
+import ComponentB from "./ComponentB.vue";
+export default {
+  components: {
+    ComponentB
+  },
+  methods: {
+    handleCustomEvent(payload) {
+      console.log(`Custom event fired with data: ${payload}`);
+    }
+  }
+};
+</script>
---
+++ src/components/3-components-chain/ComponentB.vue
@@ -0,0 +1,18 @@
+<template>
+  <ComponentC :message="message" @customEventName="handleCustomEvent" />
+</template>
+
+<script>
+import ComponentC from "./ComponentC.vue";
+export default {
+  props: ["message"],
+  components: {
+    ComponentC
+  },
+  methods: {
+    handleCustomEvent(payload) {
+      this.$emit("customEventName", payload);
+    }
+  }
+};
+</script>
