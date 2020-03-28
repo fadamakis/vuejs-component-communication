8faaa01c37bce1dbe9ae744bd41e9fbf86683e64
Fotis Adamakis <fotis316@yahoo.com>
2020-03-28 16:50:03 +0200

    2 component communication

---
+++ src/components/2-components/ComponentB.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <p>Passed data: {{ message }}</p>
+    <button @click="clickHandler">Send event</button>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ["message"],
+  methods: {
+    clickHandler() {
+      this.$emit("customEventName", "pong");
+    }
+  }
+};
+</script>
