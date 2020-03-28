76f350d1cb5e1dae2ba9b843b5204151090d840e
Fotis Adamakis <fotis316@yahoo.com>
2020-03-28 16:50:33 +0200

    3 sibling component communication

---
+++ src/components/3-components-siblings/ComponentA.vue
@@ -0,0 +1,28 @@
+pol<template>
+  <section>
+    <ComponentB @customEventName="handleCustomEvent" />
+    <ComponentC :message="message" />
+  </section>
+</template>
+
+<script>
+import ComponentB from "./ComponentB.vue";
+import ComponentC from "./ComponentC.vue";
+export default {
+  data() {
+    return {
+      message: "ping"
+    };
+  },
+  components: {
+    ComponentB,
+    ComponentC
+  },
+  methods: {
+    handleCustomEvent(payload) {
+      console.log(`Custom event fired with data: ${payload}`);
+      this.message = payload;
+    }
+  }
+};
+</script>
